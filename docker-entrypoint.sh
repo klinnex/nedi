2ccb4d523986e76f6ee480edc949e994e3dbf0f7
Klinnex <yann.deboeuf@gmail.com>
2021-04-10 10:39:24 +0200

    Test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,12 @@
 
 # Create user
 sed -i 's#dbpa55#'${MYSQL_ROOT_PASSWORD}'#g' /var/local/nedi/nedi.conf
+CONTAINER_ALREADY_STARTED="/var/local/nedi/already_first_start.pid"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    # YOUR_JUST_ONCE_LOGIC_HERE
+else
 /var/local/nedi/install.exp $DBUSER $MYSQL_ROOT_PASSWORD
-
+sleep 60
+fi
